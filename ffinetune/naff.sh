f51140fd7cf9586747802eec4d31d528b83931ab
sili <siliwang94@gmail.com>
2023-08-06 22:22:13 +0200

    new sh

---
+++ ffinetune/naff.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=siliwang94@gmail.com
+#SBATCH --partition=bme.gpustudent.q
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:1
+#SBATCH --time=168:00:0
+#SBATCH --job-name=nafff
+#SBATCH --output=/home/bme001/20225898/OC_classification/ffinetune/result/naff.out
+
+module load cuda11.6/toolkit
+
+source ~/.bashrc
+source /home/bme001/20225898/miniconda3/etc/profile.d/conda.sh
+conda activate StableDiffusion
+
+cd /home/bme001/20225898/OC_classification/ffinetune/
+srun python naff.py
