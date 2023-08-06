4a8b5bd3a3305ef9590001be4afcae39a4b5f672
sili <siliwang94@gmail.com>
2023-08-06 21:12:52 +0200

    new sh

--- crawler/88mall.py
+++ crawler/88mall.py
@@ -43,7 +43,7 @@ class BertClassifier(nn.Module):
     def __init__(self, dropout=0.5):
         super(BertClassifier, self).__init__()
         # self.bert = BertModel.from_pretrained('roberta-base')
-        self.bert = BertModel.from_pretrained('bert-base-uncased_layers0_1_2_3_4_5_6_7_8_9_10_11_model.pt')
+        self.bert = BertModel.from_pretrained('bert-base-multilingual-uncased_layers0_1_2_3_4_5_6_7_8_9_10_11_model.pt')
         self.dropout = nn.Dropout(dropout)
         self.linear = nn.Linear(768, 388)
         self.relu = nn.ReLU()
